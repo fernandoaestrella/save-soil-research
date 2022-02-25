a2f4c278599cbc4efe4d5a6991118b3175681d85
Fernando Estrella <fernandoaestrella@gmail.com>
2022-02-24 23:44:04 -0400

    removed credential

--- scratchpad.py
+++ scratchpad.py
@@ -1,10 +1,9 @@
-# https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername=NattiNatasha&key=AIzaSyCcSfqn6EYEwMpSBpMa84d3mQWAX8TEwAY
 import urllib.request
 import json
 
 # name = input("Enter username: ")
 name = 'vessi'
-key = "AIzaSyBRMDZ-vBHk4JDveOZrprmAeN8L359laW0"
+key = ""
 data = urllib.request.urlopen(
     "https://www.googleapis.com/youtube/v3/channels?&forUsername="+name+"&key="+key).read()
 # subs = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
